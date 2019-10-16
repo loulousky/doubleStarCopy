16aa146ef7d31e0bfa7cc10a23d1302fc7c5d3bd
loulousky <185587041@qq.com>
2019-10-16 18:04:07 +0800

    首页面功能实现中

--- submodels/homemodel/src/main/java/com/example/liuhai/homemodel/controller/LHFindListModel.kt
+++ submodels/homemodel/src/main/java/com/example/liuhai/homemodel/controller/LHFindListModel.kt
@@ -1,8 +1,8 @@
 package com.example.liuhai.homemodel.controller
 
 import android.app.Application
-import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.*
+import com.example.liuhai.homemodel.bean.LHFindBean
 
 /**
  * 作者：liuhai
@@ -11,30 +11,78 @@ import androidx.lifecycle.MutableLiveData
  * 说明：列表页的
  * type返回列表的数据分类
  */
-class LHFindListModel(val type: String, application: Application) : AndroidViewModel(application) {
+class LHFindListModel(val type: String, application: Application) : AndroidViewModel(application){
+
+    /**
+     * 懒加载
+     */
+    private  var pageNo:MutableLiveData<Int>? = null
+          get() {
+            if(field==null){
+
+                field= MutableLiveData(-1)
+             }
+              return field
+         }
+
+    //页面数据集合 页面只要监听这个数据集合即可
+    val findlist: MediatorLiveData<MutableList<LHFindBean>> =
+        MediatorLiveData<MutableList<LHFindBean>> ().apply {
+            this.addSource(pageNo!!){
+                if(it==0) {
+                    this.value = loadDataWithPage(it)
+                }else{
+
+                    this.value?.addAll(loadDataWithPage(it))
+                }
+            }
+        }
 
 
-
-
-
-
-
-
-
-
-
-
-    interface LHView<T : Any> {
-        //加载数据
-        fun loadData(): List<T>
-
-        //刷新数据
-        fun refushData(): List<T>
-
-        //添加新数据
-        fun moreData(): List<T>
+    /**
+     * 下一页
+     * 页数加一通知数据更新一页并添加到当前的列表中
+     * 页面中只要监听了list数据更新reclview即可
+     */
+    fun nextPage() {
+        pageNo?.postValue(pageNo?.value!!+1)
 
     }
 
 
+    /**
+     * 第一次加载。或者下拉刷新的加载
+     */
+   fun loadList(){
+       pageNo?.value=0
+
+   }
+
+
+    /**
+     * 加载下一页
+     */
+    private fun loadDataWithPage(page:Int):MutableList<LHFindBean>{
+        val data:MutableList<LHFindBean> =ArrayList<LHFindBean>()
+        for ( i in 0..19){
+            data.add(  LHFindBean("测试账号${i}","测试描述${i}","","",false,i))
+        }
+        return data
+   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
+
